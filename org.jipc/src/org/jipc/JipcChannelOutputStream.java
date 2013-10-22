e7c9edc9b1bad797feae497c126969b6d3458d19
Ralf Stuckert <ralf.stuckert@googlemail.com>
2013-10-22 14:10:30 +0200

    Introduced WritableChannelOutputStream

---
+++ org.jipc/src/org/jipc/JipcChannelOutputStream.java
@@ -0,0 +1,59 @@
+package org.jipc;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+
+public class JipcChannelOutputStream extends OutputStream {
+
+	private static final int SLEEP_TIME = 100;
+
+	private WritableJipcByteChannel channel;
+	private ByteBuffer oneByteBuffer = ByteBuffer.wrap(new byte[1]);
+
+	public JipcChannelOutputStream(final WritableJipcByteChannel channel) {
+		if (channel == null) {
+			throw new IllegalArgumentException(
+					"parameter 'channel' must not be  null");
+		}
+		this.channel = channel;
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		oneByteBuffer.clear();
+		oneByteBuffer.put((byte) b);
+		int count = 0;
+		while ((count = channel.write(oneByteBuffer)) == 0
+				&& !channel.isClosedByPeer()) {
+			sleep();
+		}
+		if (count == 0) {
+			throw new ClosedChannelException();
+		}
+	}
+
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		ByteBuffer buffer = ByteBuffer.wrap(b, off, len);
+		int count = 0;
+		while ((count = channel.write(buffer)) == 0
+				&& !channel.isClosedByPeer()) {
+			sleep();
+		}
+		if (count == 0) {
+			throw new ClosedChannelException();
+		}
+	}
+
+	protected void sleep() throws InterruptedIOException {
+		try {
+			Thread.sleep(SLEEP_TIME);
+		} catch (InterruptedException e) {
+			throw new InterruptedIOException(e.getMessage());
+		}
+	}
+
+}
---
+++ org.jipc/src/org/jipc/WritableJipcByteChannel.java
@@ -0,0 +1,8 @@
+package org.jipc;
+
+import java.nio.channels.WritableByteChannel;
+
+public interface WritableJipcByteChannel extends JipcChannel,
+		WritableByteChannel {
+
+}
