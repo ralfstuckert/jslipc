9d4337de31d9389a6d94acda0241e75d44a103ee
Ralf Stuckert <ralf.stuckert@googlemail.com>
2013-10-29 17:59:57 +0100

    added a lil' demo

---
+++ org.jipc/test/demo/Consumer.java
@@ -0,0 +1,42 @@
+package demo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import org.jipc.JipcRole;
+import org.jipc.channel.JipcChannelInputStream;
+import org.jipc.channel.JipcChannelOutputStream;
+import org.jipc.ipc.file.FilePipe;
+
+public class Consumer {
+
+	public static void main(String[] args) throws Exception {
+		// set up pipe
+		File directory = new File("./pipe");
+		directory.mkdir();
+		FilePipe pipe = new FilePipe(directory, JipcRole.Yang);
+		pipe.cleanUpOnClose();
+		// set up streams
+		OutputStream out = new JipcChannelOutputStream(pipe.sink());
+		InputStream in = new JipcChannelInputStream(pipe.source());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+		
+		// receive message from producer
+		String received = reader.readLine();
+		System.out.println("producer asked: '" + received + "'" );
+		// send message to producer
+		System.out.println("answering: 'I'm fine'" );
+		writer.write("I'm fine\n");
+
+		// close all resources
+		reader.close();
+		writer.close();
+		pipe.close();
+	}
+}
